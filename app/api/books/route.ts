fef9a2f57d74a82b79dbc3285a9c2ededfd69417
Azura <saamueel64@outlook.es>
2024-06-20 21:17:38 -0600

    chore: Update .gitignore, set up prisma scheme and create route for API

---
+++ app/api/books/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
+
+// Fetch all books
+export async function GET(request: NextRequest) {
+    
+}
+
+// Create a new book
+export async function POST(request: NextRequest) {
+    
+}
